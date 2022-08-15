80a47bc8aa721ffa7616b9f48239f92571ac5a5f
anhhv492 <anhhv492@gmail.com>
2022-08-15 17:37:44 +0700

    Revert "delete file class"
    This reverts commit fb037489a72385153ca32677e5b25398ccf64a86.

---
+++ JavaCore/basic1/src/main/java/StringExample2.java
@@ -0,0 +1,15 @@
+public class StringExample2{
+	public static void main(String[]args){
+		//1
+		char[]chars={'h','e','i','l','o'};
+		String text=new String(chars);
+		System.out.println(text);
+		//2
+		text=text.concat(" ");
+		text=text.concat(args[0]);
+		System.out.println("new value is "+text);
+		//3
+		System.out.println("new value is "+text);
+		System.out.println(args[0].equals("as"));
+	}
+}
---
+++ JavaCore/basic2/src/main/java/ThrowableExample.java
@@ -0,0 +1,11 @@
+public class ThrowableExample {
+//    String value="!2";
+//    int SaiSoException{
+//        try{
+//            Integer integer=Integer.parseInt(value);
+//            return integer.intValue();
+//        }catch(NumberFormatException e){
+//            throw new SaiSoException(value);
+//        }
+//    }
+}
